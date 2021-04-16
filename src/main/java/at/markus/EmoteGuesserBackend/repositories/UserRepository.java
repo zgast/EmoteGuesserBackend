9e6db3d27dc37d2572d3fad9e49c875bb982c62b
Jake Esser <x7airworker@gmail.com>
2021-04-16 12:18:10 +0200

    Removed unused imports of repository

--- src/main/java/at/markus/EmoteGuesserBackend/repositories/UserRepository.java
+++ src/main/java/at/markus/EmoteGuesserBackend/repositories/UserRepository.java
@@ -1,10 +1,7 @@
 package at.markus.EmoteGuesserBackend.repositories;
 
 import at.markus.EmoteGuesserBackend.document.User;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
-
-import java.util.List;
 
 public interface UserRepository extends MongoRepository<User, String> {
 }
